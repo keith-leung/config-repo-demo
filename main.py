e34c22909c8a48fbec46a0862dc406b550ec0e3f
keith-leung <gzkeith@live.cn>
2025-12-03 14:11:38 -0500

    testing adding files

--- main.py
+++ main.py
@@ -14,3 +14,64 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Best Partners TV
+https://www.youtube.com/@bestpartners
+
+# Peter Pang -- Architect
+https://www.youtube.com/@yuan_zi_neng
+
+# Tiff In Tech
+https://www.youtube.com/@TiffInTech
+
+# Gary Explains
+https://www.youtube.com/@GaryExplains
+
+# Leah's English 莉雅老师
+https://www.youtube.com/@Leah%E7%9A%84%E5%B0%8F%E6%97%A5%E8%AE%B0
+
+# Anthropic
+https://www.youtube.com/@anthropic-ai
+
+# English with Lucy
+https://www.youtube.com/@EnglishwithLucy
+
+# 出口日語
+https://www.youtube.com/@deguchi
+
+# Alexander Rotmensz
+https://www.youtube.com/@alexanderrotmensz
+
+# Stanford Online
+https://www.youtube.com/@stanfordonline
+
+# freeCodeCamp.org
+https://www.youtube.com/@freecodecamp
+
+# Zoe Languages
+https://www.youtube.com/@zoe.languages
+
+# Tech Unicorn
+https://www.youtube.com/@Tech.Unicorn
+
+# Smashing English! Free and Fun English Lessons!
+https://www.youtube.com/@smashingenglish
+
+# 老石谈芯 Shilicon Talk
+https://www.youtube.com/@laoshi_tec
+
+# ScaleFlux
+https://www.youtube.com/@scaleflux3235
+
+# zaharaEnglish
+https://www.youtube.com/@zaharaEnglish
+
+# Tech With Nikola
+https://www.youtube.com/@TechWithNikola
+
+# CXL Consortium
+https://www.youtube.com/@CXLConsortium
+
+
+
+
